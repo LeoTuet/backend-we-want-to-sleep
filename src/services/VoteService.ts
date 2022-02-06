162a428948863b6421038cb9c80ae4c4776b2db5
94726 <maik.s.kowol@gmail.com>
2022-02-06 19:06:26 +0100

    #7 adds services

--- src/services/VoteService.ts
+++ src/services/VoteService.ts
@@ -1,7 +1,27 @@
+import {Vote} from "../repositories/schemas";
+import BallotRepository from "../repositories/BallotRepository";
+import VoteRepository from "../repositories/VoteRepository";
+import {ObjectId} from "mongodb";
+
 export class VoteService {
 
-    public saveVote() {
+  public async saveVote(ballotID: string, token: string, vote: string) {
+    VoteRepository.addVote(
+      token,
+      (await BallotRepository.getBallot(ballotID))._id,
+      vote
+    )
+  }
 
-    }
+  public async getVote(ballotID: string, token: string): Promise<Vote> {
+    return await VoteRepository.getVote(token, new ObjectId(ballotID))
+  }
 
+  public async checkIfAlreadyVoted(ballotID: string, token: string): Promise<boolean> {
+    return (await this.getVote(ballotID, token)) != null
+  }
+
+  public async checkIfVoteOptionValid(ballotID: string, vote: string): Promise<boolean> {
+    return (await BallotRepository.getBallot(ballotID)).options.includes(vote)
+  }
 }
