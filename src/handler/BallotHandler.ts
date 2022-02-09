351b9049a2f17230a923d851ad5547fe973121f7
Luis Q. Haitzer <luis@haitzer.de>
2022-02-09 12:53:08 +0100

    improving some endpoints

--- src/handler/BallotHandler.ts
+++ src/handler/BallotHandler.ts
@@ -5,16 +5,17 @@ const ballotService = new BallotService()
 
 export class BallotHandler {
   public async addBallot(running: boolean, options: string[]) {
-    if (options.length < 2) throw createError(422 , "Not enough vote options")
-    await ballotService.addBallot(running, options)
+    if (options.length < 2) throw createError(422, "Not enough vote options");
+    await ballotService.addBallot(running, options);
   }
 
   public async deleteBallot(ballotID: string) {
-    if (!await ballotService.checkIfBallotIDExists(ballotID)) throw createError(400, "Ballot with given id does not exist")
-    await ballotService.deleteBallot(ballotID)
+    if (!(await ballotService.checkIfBallotIDExists(ballotID)))
+      throw createError(400, "Ballot with given id does not exist");
+    await ballotService.deleteBallot(ballotID);
   }
 
-  public getBallots = ballotService.getBallots
+  public getBallots = ballotService.getBallots;
 
-  public getRunningBallots = ballotService.getRunningBallots
+  public getRunningBallot = ballotService.getRunningBallot;
 }
--- src/repositories/BallotRepository.ts
+++ src/repositories/BallotRepository.ts
@@ -1,25 +1,27 @@
-import {getCollection} from "./connectToDB";
-import {Ballot} from "./schemas";
-import {ObjectId} from "mongodb";
+import { getCollection } from "./connectToDB";
+import { Ballot } from "./schemas";
+import { ObjectId } from "mongodb";
 
 export default {
   addBallot(running: boolean, options: string[]) {
-    getCollection("ballot").insertOne({running, options})
+    getCollection("ballot").insertOne({ running, options });
   },
 
   deleteBallot(ballotID: string) {
-    getCollection("ballot").deleteOne({_id: new ObjectId(ballotID)})
+    getCollection("ballot").deleteOne({ _id: new ObjectId(ballotID) });
   },
 
   async getBallot(ballotID: string): Promise<Ballot> {
-    return await getCollection("ballot").findOne({_id: new ObjectId(ballotID)})
+    return await getCollection("ballot").findOne({
+      _id: new ObjectId(ballotID),
+    });
   },
 
   async getBallots(): Promise<Ballot[]> {
-    return await getCollection<Ballot>("ballot").find().toArray()
+    return await getCollection<Ballot>("ballot").find().toArray();
   },
 
-  async getRunningBallots(): Promise<Ballot[]> {
-    return await getCollection<Ballot>("ballot").find({running: true}).toArray()
-  }
-}
+  async getRunningBallot(): Promise<Ballot> {
+    return await getCollection<Ballot>("ballot").findOne({ running: true });
+  },
+};
