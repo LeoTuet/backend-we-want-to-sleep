37809a770679ed9fa8608eed300299c4aad392b6
LeoTuet <leopold@tueting.net>
2022-02-10 22:34:15 +0100

    moves url out of function and differentiates console error with if

--- src/middleware/CaptchaMiddleware.ts
+++ src/middleware/CaptchaMiddleware.ts
@@ -4,14 +4,14 @@ import createError from "http-errors";
 import {asyncHandler} from "../utils/AsyncHandler";
 import {NextFunction, Request} from "express";
 
+const url = "https://hcaptcha.com/siteverify";
+
 export const isCaptchaValid = asyncHandler(async (req: Request<{}, {}, {}>, res, next: NextFunction) => {
     const captchaToken = req.headers['x-captcha'];
-    const url = "https://hcaptcha.com/siteverify";
 
     if (!captchaToken) {
         throw createError(403, "Captcha token missing");
     }
-
     const data = new URLSearchParams([
         ["response", captchaToken.toString()],
         ["secret", secrets.HCAPTCHA_SECRET]
@@ -24,7 +24,11 @@ export const isCaptchaValid = asyncHandler(async (req: Request<{}, {}, {}>, res,
     });
     const jsonResponse = (await response.json()) as HCaptchaRespone;
     if (!response.ok || !jsonResponse.success) {
-        console.error("Captcha token was not valid. Reason: " + (jsonResponse["error-codes"] || ("Captcha API responded with: " + response.status)))
+        if (jsonResponse["error-codes"]) {
+            console.error(`Captcha token was not valid with reason: ${jsonResponse["error-codes"]}`)
+        } else {
+            console.error(`Captcha API responded with: ${response.status}`)
+        }
         throw createError(403, "Captcha token was not valid")
     }
 
