b96795111d33bf3a0e91e751aea901f1792a2143
Leo <leopold@tueting.net>
2022-02-10 09:43:11 +0100

    #6 adds asymmetric key signing and verification

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import logger from "jet-logger";
 import app from "./app";
-import { connectToDB } from "./repositories/connectToDB";
+import {connectToDB} from "./repositories/connectToDB";
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
 app.listen(port, () => {
-  logger.info("Express server started on port: " + port);
+    logger.info("Express server started on port: " + port);
 });
 
 connectToDB();
--- src/utils/JWTs.ts
+++ src/utils/JWTs.ts
@@ -1,25 +1,28 @@
 import jwt from "jsonwebtoken"
 import {secrets} from "./secrets";
 
-const JWTSecret = secrets.JWTSecret
+const jwtPrivateKey = Buffer.from(secrets.JWTPrivateKey, "utf-8")
+const jwtPublicKey = Buffer.from(secrets.JWTPublicKey, "utf-8")
+
+const algorithm = "ES256"
 
 export default {
-  verifyAccessToken(token: string): boolean {
-    try {
-      jwt.verify(token, JWTSecret)
-    } catch {
-      return false
+    verifyAccessToken(token: string): boolean {
+        try {
+            jwt.verify(token, jwtPublicKey, {algorithms: [algorithm]})
+        } catch {
+            return false
+        }
+        return true
+    },
+
+    // Remember to use try catch to verify token validity
+    verifyAndDecodeAccessToken(token: string) {
+        return jwt.verify(token, jwtPublicKey, {complete: true, algorithms: [algorithm]})
+    },
+
+
+    generateAccessToken(username: string): string {
+        return jwt.sign({username}, jwtPrivateKey, {expiresIn: "1h", algorithm: algorithm})
     }
-    return true
-  },
-
-  // Remember to use try catch to verify token validity
-  verifyAndDecodeAccessToken(token: string) {
-    return jwt.verify(token, JWTSecret, {complete: true})
-  },
-
-
-  generateAccessToken(username: string): string {
-    return jwt.sign({username}, JWTSecret, {expiresIn: "1h"})
-  }
 }
