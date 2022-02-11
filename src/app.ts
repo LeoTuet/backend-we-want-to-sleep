a4e0ef97b01fe5b52ce32aa68fb98b1790a54538
Marc Schwaiger <marcschwaiger@outlook.de>
2022-02-11 09:06:17 +0100

    added rate limiter

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,35 @@ import compression from 'compression';
 import express, {Request, Response, NextFunction} from 'express';
 import baseRouter from './routes';
 import {isHttpError} from "http-errors";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.set('trust proxy', 1) // so we don't block nginx reverse proxy
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+// Apply the rate limiting to all requests
+app.use(limiter);
+
+const adminLoginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+// Apply the rate limiting to admin login requests
+app.use("/admin/login", adminLoginLimiter);
 
 app.use(baseRouter);
 
