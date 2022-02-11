7abb74a69246c47cf835aeeb575c591464386dce
Leo <leopold@tueting.net>
2022-02-11 07:38:13 +0100

    #6 adds check if Admin was really deleted

--- src/repositories/AdminRepository.ts
+++ src/repositories/AdminRepository.ts
@@ -1,16 +1,20 @@
 import {getCollection} from "./connectToDB";
 import {Admin} from "./schemas";
+import createError from "http-errors";
 
 export default {
-  addAdmin(username: string, passwordHash: string) {
-    getCollection("admin").insertOne({username, passwordHash});
-  },
+    addAdmin(username: string, passwordHash: string) {
+        getCollection("admin").insertOne({username, passwordHash});
+    },
 
-  deleteAdmin(username: string) {
-    getCollection("admin").deleteOne({username});
-  },
+    async deleteAdmin(username: string) {
+        const result = await getCollection("admin").deleteOne({username})
+        if (result.deletedCount !== 1) {
+            throw createError(500, "Unable to delete Admin")
+        }
+    },
 
-  async getAdmin(username: string): Promise<Admin> {
-    return await getCollection<Admin>("admin").findOne({username})
-  }
+    async getAdmin(username: string): Promise<Admin> {
+        return await getCollection<Admin>("admin").findOne({username})
+    }
 };
