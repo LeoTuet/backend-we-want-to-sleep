1e50d2e58fa4f4e5dc85188ad531033b6558f817
LeoTuet <leopold@tueting.net>
2022-02-15 20:38:24 +0100

    unifies repositories

--- src/repositories/TokenRepository.ts
+++ src/repositories/TokenRepository.ts
@@ -2,13 +2,16 @@ import {getCollection} from "./connectToDB";
 import {Token} from "./schemas";
 
 export default {
-  addToken(token: string, valid: boolean, createdBy: string, createdAt: Date) {
-    getCollection("token").insertOne({
+  async addToken(token: string, valid: boolean, createdBy: string, createdAt: Date) {
+    const result = await getCollection("token").insertOne({
       token: token,
       valid: valid,
       createdBy,
       createdAt
     });
+    if (!result.acknowledged) {
+      throw Error("Token could not be created")
+    }
   },
 
   async addTokens(tokens: Omit<Token, "_id">[]) {
@@ -17,8 +20,11 @@ export default {
       throw Error("Amount of inserted tokens did not match the amount of given tokens")
   },
 
-  deleteToken(token: string) {
-    getCollection("token").deleteOne({token});
+  async deleteToken(token: string) {
+    const result = await getCollection("token").deleteOne({token});
+    if (result.deletedCount !== 1) {
+      throw Error("Token could not be deleted")
+    }
   },
 
   async getToken(token: string): Promise<Token> {
