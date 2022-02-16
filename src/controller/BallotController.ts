424499ed0bed1dc852566a0358bac3525667b524
94726 <maik.s.kowol@gmail.com>
2022-02-16 23:34:59 +0100

    optimizes ballot handling

--- src/controller/BallotController.ts
+++ src/controller/BallotController.ts
@@ -2,10 +2,20 @@ import { Request } from "express";
 import { asyncHandler } from "../utils/AsyncHandler";
 import { BallotHandler } from "../handler/BallotHandler";
 import { Ballot } from "../repositories/schemas";
+import Joi from "joi";
 
 const ballotHandler = new BallotHandler();
 
 type CreationBallot = Omit<Ballot, "_id">
+
+const CreateBallotSchema = Joi.object().keys({
+  running: Joi.boolean().required().strict(),
+  question: Joi.string().required(),
+  options: Joi.array().min(2).items(Joi.object().keys({
+    identifier: Joi.string().required(),
+    label: Joi.string().required()
+  }))
+});
 
 export default {
   list: asyncHandler(async (req: Request<{}, {}, {}>, res) => {
@@ -17,13 +27,14 @@ export default {
   }),
 
   add: asyncHandler(async (req: Request<{}, {}, CreationBallot, {}, {username: string}>, res) => {
+    Joi.assert(req.body, CreateBallotSchema)
     res.json({
       data: await ballotHandler.addBallot(req.body.running, req.res.locals.username, req.body.question, req.body.options),
     });
   }),
 
   delete: asyncHandler(
-    async (req: Request<{}, {}, { id: string; password: string }>, res) => {
+    async (req: Request<{}, {}, { id: string }>, res) => {
       res.json({ data: await ballotHandler.deleteBallot(req.body.id) });
     }
   ),
--- src/handler/BallotHandler.ts
+++ src/handler/BallotHandler.ts
@@ -1,12 +1,15 @@
 import {NotFound, UnprocessableEntity} from "http-errors";
 import { VotingOption } from "../repositories/schemas";
 import { BallotService } from "../services/BallotService";
+import {AdminService} from "../services/AdminService";
 
 const ballotService = new BallotService();
+const adminService = new AdminService();
 
 export class BallotHandler {
   public async addBallot(running: boolean, createdBy: string, question: string, options: VotingOption[]) {
     if (options.length < 2) throw new UnprocessableEntity("Not enough vote options");
+    if (!await adminService.checkIfUsernameExists(createdBy)) throw new UnprocessableEntity("There is no admin with the given username");
     await ballotService.addBallot(running, createdBy, question, options);
   }
 
