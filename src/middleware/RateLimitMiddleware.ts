ebcadf57bf90685a5d06e52c437af8e9bbdc6046
94726 <maik.s.kowol@gmail.com>
2022-04-10 23:37:03 +0200

    #50 updates rate limit to follow error schema

--- src/middleware/RateLimitMiddleware.ts
+++ src/middleware/RateLimitMiddleware.ts
@@ -1,15 +1,26 @@
 import rateLimit from "express-rate-limit";
 
+const defaultMessage = {
+  error: {
+    timestamp: new Date(),
+    status: 429,
+    error: "TooManyRequests",
+    message: "Too many requests, please try again later.",
+  },
+};
+
 export const defaultLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100,
-    standardHeaders: true,
-    legacyHeaders: false,
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: defaultMessage,
 });
 
 export const adminLoginLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5,
-    standardHeaders: true,
-    legacyHeaders: false,
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: defaultMessage
 });
