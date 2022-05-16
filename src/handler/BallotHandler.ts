647eca13849d5776addc961c95e8ad34684da183
Schwaiger, Marc <Marc.Schwaiger@adesso.de>
2022-05-16 15:20:52 +0200

    #52 Added functions to count votes

--- src/handler/BallotHandler.ts
+++ src/handler/BallotHandler.ts
@@ -1,10 +1,22 @@
 import { NotFound, UnprocessableEntity } from "http-errors";
 import { VotingOption } from "../repositories/schemas";
 import { BallotService } from "../services/BallotService";
+import { VoteService } from "../services/VoteService";
 import { AdminService } from "../services/AdminService";
 
 const ballotService = new BallotService();
 const adminService = new AdminService();
+const voteService = new VoteService();
+
+export interface VoteResult {
+  questionIdentifier: string,
+  questionLabel: string,
+  amount: number
+}
+
+export interface TotalVoteCount {
+  count: number
+}
 
 export class BallotHandler {
   public async addBallot(
@@ -46,5 +58,53 @@ export class BallotHandler {
       throw new UnprocessableEntity("Not enough vote options");
 
     await ballotService.updateBallot(ballotID, running, question, options);
+  }
+
+  public async getAdminStatus(
+    ballotID: string
+  ): Promise<VoteResult[]> {
+    if (!(await ballotService.checkIfBallotIDExists(ballotID)))
+      throw new NotFound("Ballot with given id does not exist");
+
+    const ballot = await ballotService.getBallot(ballotID);
+    const votes = await voteService.getVotes(ballotID);
+
+    const results: Record<string, number> = {} as never
+
+    // init vote counter
+    for (const voteOption of ballot.options) {
+      results[voteOption.identifier] = 0
+    }
+
+    // count votes
+    for (const vote of votes) {
+      results[vote.vote]++;
+    }
+
+    // fill records into array list
+    const out: VoteResult[] = [];
+    for (const result of Object.getOwnPropertyNames(results)) {
+      const voteOption = ballot.options.find(vo => result == vo.identifier)
+      const vr: VoteResult = {
+        questionIdentifier: voteOption.identifier,
+        questionLabel: voteOption.label,
+        amount: results[result]
+      }
+      out.push(vr);
+    }
+    return out;
+  }
+
+  public async getTotalVoteCount(
+    ballotID: string
+  ): Promise<TotalVoteCount> {
+    if (!(await ballotService.checkIfBallotIDExists(ballotID)))
+      throw new NotFound("Ballot with given id does not exist");
+
+    const votes = await voteService.getVotes(ballotID);
+
+    return {
+      count: votes.length
+    } as TotalVoteCount;
   }
 }
