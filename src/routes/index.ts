d2fec9dcd977e74bea005e7d964972bc155d3725
Schwaiger Marc <marcschwaiger@outlook.de>
2022-05-21 12:04:23 +0200

    #55 Rearranged base router configuration to be more readable
    Signed-off-by: Schwaiger Marc <marcschwaiger@outlook.de>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,13 +10,18 @@ import {
   defaultLimiter,
 } from "../middleware/RateLimitMiddleware";
 
+const baseRouter = Router();
+baseRouter.use(defaultLimiter);
+
 // User-route
 const voteRouter = Router();
+baseRouter.use("/vote", voteRouter);
 voteRouter.post("/:ballotID", isCaptchaValid, VoteController.add);
 voteRouter.get("/:ballotID", isAdmin, VoteController.listByBallot);
 voteRouter.get("/:ballotID/:token", VoteController.getByToken);
 
 const ballotRouter = Router();
+baseRouter.use("/ballot", ballotRouter);
 ballotRouter.get("/", BallotController.list);
 ballotRouter.post("/", isAdmin, BallotController.add);
 ballotRouter.delete("/", isAdmin, BallotController.delete);
@@ -26,19 +31,15 @@ ballotRouter.get("/result/:ballotID", isAdmin, BallotController.getVoteResult);
 ballotRouter.get("/status/:ballotID", BallotController.getTotalVoteCount);
 
 const tokenRouter = Router();
+baseRouter.use("/token", tokenRouter);
 tokenRouter.get("/status/:ballotID/:token", TokenController.getStatus);
 tokenRouter.post("/", isAdmin, TokenController.generate);
 
 const adminRouter = Router();
+baseRouter.use("/admin", adminRouter);
 adminRouter.post("/login", adminLoginLimiter, AdminController.login);
 adminRouter.post("/", isAdmin, AdminController.add);
 adminRouter.delete("/", isAdmin, AdminController.delete);
 
 // Export the base-router
-const baseRouter = Router();
-baseRouter.use(defaultLimiter);
-baseRouter.use("/vote", voteRouter);
-baseRouter.use("/ballot", ballotRouter);
-baseRouter.use("/token", tokenRouter);
-baseRouter.use("/admin", adminRouter);
 export default baseRouter;
